649d08c951fe330d25a302b4460bb9b2f3b2b140
weihung <drum831@gmail.com>
2025-08-15 17:06:23 +0800

    temp

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className='pt-20 min-h-screen flex items-center justify-center'>
+      <p className='text-2xl text-neutral-600 dark:text-neutral-300'>開發中 ...</p>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { SimpleNavbarWithHoverEffects } from './Component/ui/Navbar';
+import { SimpleFooterWithFourGrids } from './Component/ui/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,8 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang='en' className='dark'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SimpleNavbarWithHoverEffects />
+        {children}
+        <SimpleFooterWithFourGrids />
+      </body>
     </html>
   );
 }
