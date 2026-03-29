bc820b72f462a31c029e6a4ad123caadbff26928
weihung0831 <drum831@gmail.com>
2026-03-29 20:31:05 +0800

    chore: add dependencies and update config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'keyboard-shop-api.zeabur.app',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
     ],
   },
 };
